a462477cdc9790b1caf3866860f85ea678f01d5b
Meghan Stang <mstang15@gmail.com>
2019-10-07 11:17:23 -0600

    Add Nested Resources Lesson

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,4 +6,20 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
   end
+
+  def new
+    @artist_id = params[:artist_id]
+  end
+
+  def create
+    artist = Artist.find(params[:artist_id])
+    song = artist.songs.create(song_params)
+    redirect_to "/artists/#{artist.id}"
+  end
+
+  private
+
+  def song_params
+    params.permit(:title,:length,:play_count)
+  end
 end
